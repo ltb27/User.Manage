87b46f9b3c64f7e31816a8938b5897c8ac0b9ec6
LTB <letuanbao@mbg8.onmicrosoft.com>
2023-10-01 22:14:01 +0700

    add and update "Roles Seeding" migration

---
+++ UserManage.API/Migrations/20231001151256_Roles Seeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManage.API.Migrations
+{
+    public partial class RolesSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "369949e2-4533-43eb-8f66-2a20c1e31f75", "3", "HR", "Human Resource" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "412a985a-a314-4fdd-818d-ffb9f71b210d", "1", "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "68f5296e-e6f9-4ca8-852c-b768c3f9b7b8", "2", "User", "User" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "369949e2-4533-43eb-8f66-2a20c1e31f75");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "412a985a-a314-4fdd-818d-ffb9f71b210d");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "68f5296e-e6f9-4ca8-852c-b768c3f9b7b8");
+        }
+    }
+}
